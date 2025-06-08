a981fb4807bd3758528f576412106c979497d825
Shohei <yukawa.shouhei@gmail.com>
2025-06-09 07:05:21 +0900

    Initial Commit

---
+++ swiftUIStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUIStudy
+//
+//  Created by 湯川昇平 on 2025/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUIStudy/swiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIStudyApp.swift
+//  swiftUIStudy
+//
+//  Created by 湯川昇平 on 2025/06/09.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

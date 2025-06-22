19b9eb6d8c5e1965c4afec34e64c3f80a585d029
Shohei <yukawa.shouhei@gmail.com>
2025-06-22 20:38:59 +0900

    Multiple Sheets学習

--- swiftUIStudy/MultipleSheetBootcamp.swift
+++ swiftUIStudy/MultipleSheetBootcamp.swift
@@ -1,48 +1,46 @@
-////
-////  MultipleSheetBootcamp.swift
-////  swiftUIStudy
-////
-////  Created by 湯川昇平 on 2025/06/21.
-////
-//
-//import SwiftUI
-//
-//struct RandomModel: Identifiable {
-//    let id = UUID().uuidString
-//    let title: String
-//}
-//
-//struct MultipleSheetsBootcamp: View {
-//    
-//    @State private var selectedModel: RandomModel? = nil
-//    
-//    var body: some View {
-//        VStack(spacing: 20) {
-//            Button("ボタン 1") {
-//                selectedModel = RandomModel(title: "スクリーン 1")
-//            }
-//            
-//            Button("ボタン 2") {
-//                selectedModel = RandomModel(title: "スクリーン 2")
-//            }
-//        }
-//        
-//        .sheet(item: $selectedModel) { model in
-//            NextScreen(selectedModel: model)
-//        }
-//    }
-//}
-//
-//struct NextScreen: View {
-//    
-//    let selectedModel: RandomModel
-//    
-//    var body: some View {
-//        Text(selectedModel.title)
-//            .font(.largeTitle)
-//    }
-//}
-//
-//#Preview {
-//    MultipleSheetsBootcamp()
-//}
+
+
+import SwiftUI
+
+// データモデル（Identifiableに準拠）
+struct RandomModel: Identifiable {
+    let id = UUID().uuidString
+    let title: String
+}
+
+struct NextScreen: View {
+    let selectedModel: RandomModel // データを受け取るだけなので let にする
+    
+    var body: some View {
+        Text(selectedModel.title)
+            .font(.largeTitle)
+    }
+}
+
+struct MultipleSheetsBootcamp: View {
+    // 状態変数をオプショナル（nil許容）にする
+    @State private var selectedModel: RandomModel? = nil
+    
+    var body: some View {
+        ScrollView { // 多数のボタンを配置するためにScrollViewを使用
+            VStack(spacing: 20) {
+                ForEach(1..<51) { index in
+                    Button("Button \(index)") {
+                        // ボタンが押されたら、対応するモデルをセットする
+                        selectedModel = RandomModel(title: "\(index)")
+                    }
+                }
+            }
+        }
+        .font(.caption2)
+        // isPresentedの代わりにitemを使用
+        .sheet(item: $selectedModel) { model in
+            // itemに値がセットされると、その値がクロージャに渡される
+            NextScreen(selectedModel: model)
+        }
+    }
+}
+
+#Preview {
+    MultipleSheetsBootcamp()
+}
